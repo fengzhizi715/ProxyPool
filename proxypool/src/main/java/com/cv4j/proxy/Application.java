0126893d613327652e63bea80a7d346b9e2228d0
fengzhizi715 <fengzhizi715@126.com>
2017-11-22 19:28:28 +0800

    add ScheduleJobs

--- proxypool/src/main/java/com/cv4j/proxy/Application.java
+++ proxypool/src/main/java/com/cv4j/proxy/Application.java
@@ -1,29 +1,17 @@
 package com.cv4j.proxy;
 
-import com.cv4j.proxy.domain.Proxy;
-import com.safframework.tony.common.utils.Preconditions;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.cv4j.proxy.dao.ProxyDao;
-
-import java.util.concurrent.CopyOnWriteArrayList;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by tony on 2017/11/16.
  */
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableScheduling
 public class Application implements CommandLineRunner{
-
-    @Autowired
-    ProxyDao proxyDao;
-
-    @Autowired
-    ProxyManager proxyManager;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -31,16 +19,7 @@ public class Application implements CommandLineRunner{
 
     @Override
     public void run(String... args) throws Exception {
-//        proxyManager.start();
-//
-//        CopyOnWriteArrayList<Proxy> list = ProxyPool.proxyList;
-//
-//        if (Preconditions.isNotBlank(list)) {
-//
-//            for (Proxy p:list) {
-//
-//                proxyDao.saveProxy(p);
-//            }
-//        }
+
+        System.out.println("proxyManager.start()");
     }
 }
