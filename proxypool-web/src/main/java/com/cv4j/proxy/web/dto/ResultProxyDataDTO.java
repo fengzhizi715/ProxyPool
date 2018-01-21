ea5e92cf3b272efed86499918f73fc3caf5d0cfe
fengzhizi715 <fengzhizi715@126.com>
2018-01-21 12:04:39 +0800

    add HttpResponse

--- proxypool-web/src/main/java/com/cv4j/proxy/web/dto/ResultProxyDataDTO.java
+++ proxypool-web/src/main/java/com/cv4j/proxy/web/dto/ResultProxyDataDTO.java
@@ -1,16 +1,7 @@
 package com.cv4j.proxy.web.dto;
 
-import lombok.Data;
-
 import java.util.List;
 
-@Data
-public class ResultProxyDataDTO {
-
-    private int code;
-
-    private String message;
-
-    private List<ProxyDataDTO> data;
+public class ResultProxyDataDTO extends HttpResponse<List<ProxyDataDTO>> {
 
 }
