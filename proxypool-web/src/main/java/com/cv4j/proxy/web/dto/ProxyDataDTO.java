937b0c7f56483a0666c03397f2acc50ce10a8801
shenqinhua <31983235@qq.com>
2018-01-30 10:45:16 +0800

    update

--- proxypool-web/src/main/java/com/cv4j/proxy/web/dto/ProxyDataDTO.java
+++ proxypool-web/src/main/java/com/cv4j/proxy/web/dto/ProxyDataDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class ProxyDataDTO {
 
-    private String type;
-    private String ip;
-    private int port;
+    private String proxyType;
+    private String proxyAddress;
+    private int proxyPort;
 }
