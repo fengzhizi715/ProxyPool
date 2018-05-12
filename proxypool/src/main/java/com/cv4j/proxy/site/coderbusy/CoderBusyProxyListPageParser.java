f2ce46383650a10e0355a4ff1382cc57cef7dccc
sinkinka@126.com <Aa123456>
2018-05-12 19:46:44 +0800

    update

--- proxypool/src/main/java/com/cv4j/proxy/site/coderbusy/CoderBusyProxyListPageParser.java
+++ proxypool/src/main/java/com/cv4j/proxy/site/coderbusy/CoderBusyProxyListPageParser.java
@@ -3,6 +3,7 @@ package com.cv4j.proxy.site.coderbusy;
 
 import com.cv4j.proxy.ProxyListPageParser;
 import com.cv4j.proxy.config.Constant;
+import com.cv4j.proxy.domain.Page;
 import com.cv4j.proxy.domain.Proxy;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
@@ -13,19 +14,23 @@ import org.jsoup.select.Elements;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 解析目标：https://proxy.coderbusy.com/classical/anonymous-type/highanonymous.aspx?page=1
+ */
 @Slf4j
 public class CoderBusyProxyListPageParser implements ProxyListPageParser {
 
     @Override
     public List<Proxy> parse(String content) {
         Document document = Jsoup.parse(content);
-        Elements elements = document.select("div[class='card-table'] table tr:gt(0)");
+        Elements elements = document.select("div[class='table-responsive'] table[class='table'] tbody tr");
         List<Proxy> proxyList = new ArrayList<>(elements.size());
         for (Element element : elements){
             String ip = element.select("td:eq(0)").first().text();
-            String port  = element.select("td:eq(1)").first().text();
-            String type = element.select("td:eq(4)").first().text();
-            String isAnonymous = element.select("td:eq(6)").first().text();
+            String port  = element.select("td:eq(2)").first().text();
+            String type = element.select("td:eq(5)").first().text();
+            String isAnonymous = element.select("td:eq(7)").first().text();
+            System.out.println("ip:"+ip);
             log.debug("parse result = "+type+"://"+ip+":"+port+"  "+isAnonymous);
             if(!anonymousFlag || isAnonymous.contains("匿")){
                 proxyList.add(new Proxy(ip, Integer.valueOf(port), type, Constant.TIME_INTERVAL));
