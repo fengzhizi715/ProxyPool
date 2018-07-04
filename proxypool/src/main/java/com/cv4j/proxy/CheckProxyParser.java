d4b23bef20c8cc29f2b99d979afa02c1e44afa8c
shenqinhua <31983235@qq.com>
2018-07-04 23:07:32 +0800

    update ip3366

--- proxypool/src/main/java/com/cv4j/proxy/CheckProxyParser.java
+++ proxypool/src/main/java/com/cv4j/proxy/CheckProxyParser.java
@@ -17,11 +17,13 @@ public class CheckProxyParser {
 //        proxyMap.put("http://www.goubanjia.com/", GoubanjiaProxyListPageParser.class);
 //        proxyMap.put("http://m.66ip.cn/2.html", M66ipProxyListPageParser.class);
 //        proxyMap.put("http://www.data5u.com/", Data4uProxyListPageParser.class);
-        proxyMap.put("https://list.proxylistplus.com/Fresh-HTTP-Proxy-List-1", ProxyListPlusProxyListPageParser.class);
+//        proxyMap.put("https://list.proxylistplus.com/Fresh-HTTP-Proxy-List-1", ProxyListPlusProxyListPageParser.class);
+
+        //TODO gb2312如何处理？
+        proxyMap.put("http://www.ip3366.net/", Ip3366ProxyListPageParser.class);
 
 
         ProxyPool.proxyMap = proxyMap;
-
         ProxyManager proxyManager = ProxyManager.get();
         proxyManager.start();
 
---
+++ proxypool/src/main/java/com/cv4j/proxy/site/Ip3366ProxyListPageParser.java
@@ -0,0 +1,37 @@
+package com.cv4j.proxy.site;
+
+import com.cv4j.proxy.ProxyListPageParser;
+import com.cv4j.proxy.config.Constant;
+import com.cv4j.proxy.domain.Proxy;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * http://www.ip3366.net/
+ */
+@Slf4j
+public class Ip3366ProxyListPageParser implements ProxyListPageParser {
+
+    @Override
+    public List<Proxy> parse(String html) {
+        Document document = Jsoup.parse(html);
+        Elements elements = document.select("div[id=list] table tbody tr");
+        List<Proxy> proxyList = new ArrayList<>();
+        for (Element element : elements){
+            String ip = element.select("td:eq(0)").first().text();
+            String port  = element.select("td:eq(1)").first().text();
+            String isAnonymous = element.select("td:eq(2)").first().text();
+            String type = element.select("td:eq(3)").first().text();
+            if(!anonymousFlag || isAnonymous.contains("匿") || isAnonymous.contains("anonymous")){
+                proxyList.add(new Proxy(ip, Integer.valueOf(port), type, Constant.TIME_INTERVAL));
+            }
+        }
+        return proxyList;
+    }
+}
